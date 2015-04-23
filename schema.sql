a060d2959728b18af3386a365aa91fc44e105c78
Danni Wang <dannywang0911@gmail.com>
2015-04-23 17:58:07 -0500

    Change the length of VARCHAR

--- schema.sql
+++ schema.sql
@@ -8,35 +8,37 @@ CREATE TABLE users (
   password    VARCHAR(20) NOT NULL,
   name        VARCHAR(40) NOT NULL,
   PRIMARY KEY ( user_id ),
-  UNIQUE ( password )
+  UNIQUE ( user_name )
 );
 
 CREATE TABLE shopping_lists (
   list_id     INTEGER NOT NULL AUTO_INCREMENT,
   user_id     INTEGER NOT NULL,
   name        VARCHAR(20),
-  PRIMARY KEY ( list_id )
+  PRIMARY KEY ( list_id ),
+  FOREIGN KEY ( user_id ) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE foods (
   food_id INTEGER NOT NULL AUTO_INCREMENT,
-  name VARCHAR(40) NOT NULL,
+  name VARCHAR(60) NOT NULL,
   category VARCHAR(20) NOT NULL,
-  description VARCHAR(60),
+  description VARCHAR(160),
   PRIMARY KEY ( food_id )
 );
 
 CREATE TABLE stores (
   store_id INTEGER NOT NULL AUTO_INCREMENT,
   name VARCHAR(20) NOT NULL,
-  location VARCHAR(20),
+  location VARCHAR(80),
   PRIMARY KEY ( store_id )
 );
 
 CREATE TABLE consists(
 	list_id	INTEGER NOT NULL,
 	food_id INTEGER NOT NULL,
-	quantity INTEGER NOT NULL
+	quantity INTEGER NOT NULL,
+  FOREIGN KEY ( list_id ) REFERENCES shopping_lists(list_id) ON DELETE CASCADE
 );
 
 CREATE TABLE purchases(
@@ -48,14 +50,18 @@ CREATE TABLE purchases(
 	quantity INTEGER NOT NULL,
 	purchase_date TIMESTAMP NOT NULL DEFAULT NOW(),
 	expiration_date DATE,
-	PRIMARY KEY ( purchase_id )
+	PRIMARY KEY ( purchase_id ),
+  FOREIGN KEY ( user_id ) REFERENCES users(user_id),
+  FOREIGN KEY ( food_id ) REFERENCES foods(food_id),
+  FOREIGN KEY ( store_id ) REFERENCES stores(store_id)
 );
 
 CREATE TABLE available(
 	food_id	INTEGER NOT NULL,
 	store_id INTEGER NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
-	quantity INTEGER NOT NULL
+	quantity INTEGER NOT NULL,
+  FOREIGN KEY ( store_id ) REFERENCES stores(store_id)
 );
 
 
