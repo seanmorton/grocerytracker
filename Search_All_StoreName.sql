a80c61ab7948279c84f3095fab7c585403e4d4e2
fishball1121 <liuyutingfish@gmail.com>
2015-04-23 19:09:35 -0500

    Search queries

--- Search_All_StoreName.sql
+++ Search_All_StoreName.sql
@@ -1,8 +1,9 @@
 CREATE VIEW search_result AS
 SELECT F.food_id, F.name AS food_name, F.category, A.price, S.name AS store_name
 FROM foods F, available A, stores S
-WHERE (A.price <= 15.00) AND (A.price >= 0) AND (A.quantity > 0 )
-	 AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
+WHERE (A.price <= 200) AND ( A.price >= 0) AND (F.category = "Produce") AND (S.name = "target") 
+      AND (F.name = "Anhai Farms Golden Apples") AND (A.quantity > 0 )
+	AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
 #require for upperbound and lowerbound of the price range
 
 SELECT food_name, category, price, store_name
--- Search_Price_StoreName.sql
+++ Search_Price_StoreName.sql
@@ -1,8 +1,8 @@
 CREATE VIEW search_result AS
 SELECT F.food_id, F.name AS food_name, F.category, A.price, S.name AS store_name
 FROM foods F, available A, stores S
-WHERE (A.price <= 15.00) AND (A.price >= 0) AND (A.quantity > 0 )
-	 AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
+WHERE (A.price <= 15.00) AND ( A.price >= 0) AND (S.name = "target") AND (A.quantity > 0 )
+	AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
 #require for upperbound and lowerbound of the price range
 
 SELECT food_name, category, price, store_name
