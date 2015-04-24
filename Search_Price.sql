6ad58b96dcf485d45e6af77ee2ac09ae3afb8dd7
Danni Wang <dannywang0911@gmail.com>
2015-04-23 19:25:19 -0500

    refresh

--- Search_Price.sql
+++ Search_Price.sql
@@ -1,4 +1,11 @@
-SELECT F.name, F.category, A.price, S.name
+CREATE VIEW search_result AS
+SELECT F.food_id, F.name AS food_name, F.category, A.price, S.name AS store_name
 FROM foods F, available A, stores S
-WHERE (p <= 100) AND ( p >= 0) AND (A.quantity > 0 );
+WHERE (A.price <= 15.00) AND (A.price >= 0) AND (A.quantity > 0 )
+	 AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
 #require for upperbound and lowerbound of the price range
+
+SELECT food_name, category, price, store_name
+FROM search_result;
+
+DROP VIEW search_result;
