984f5266519fe6f7974f0df2b37f8210abd0eac8
Danni Wang <dannywang0911@gmail.com>
2015-04-23 19:23:57 -0500

    refresh

--- Search_Category.sql
+++ Search_Category.sql
@@ -1,4 +1,9 @@
-SELECT DISTINCT F.name, F.category, F.price, S.name
+CREATE VIEW search_result AS
+SELECT DISTINCT F.food_id, F.name AS food_name, F.category, A.price, S.store_id, S.name AS store_name
 FROM foods F, available A, stores S
-WHERE (F.category = "CategoryInput") AND (A.quantity > 0 );
-#CategoryInput is the input
+WHERE (F.category = "Snack") AND (A.quantity > 0 ) AND (F.food_id = A.food_id) 	AND (S.store_id = A.store_id);
+
+SELECT food_name, category, price, store_name
+FROM search_result;
+
+DROP VIEW search_result;
