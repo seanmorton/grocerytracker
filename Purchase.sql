d20ea4f78cb4860e0fb178329f807c85b232306c
Danni Wang <dannywang0911@gmail.com>
2015-04-23 20:32:03 -0500

    Fix expiration_date

--- Purchase.sql
+++ Purchase.sql
@@ -1,12 +1,8 @@
-INSERT INTO purchases (user_id, food_id, store_id, quantity, price, expiration_data)
-VALUES (2,
-	(SELECT food_id, store_id
-         FROM search_result
-	 WHERE food_id = 1),
-	5,
+INSERT INTO purchases (user_id, food_id, store_id, quantity, price, expiration_date)
+VALUES (2, 7, 1, 5,
         (SELECT price * 5
 	 FROM search_result
-	 WHERE food_id = 1),
-	 2015-5-10);
+	 WHERE (food_id = 7) AND (store_id = 1)),
+	 "2015-5-10");
 
 
