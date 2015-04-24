6e47c4985f0665cebb687ed271011b2197e66701
Danni Wang <dannywang0911@gmail.com>
2015-04-23 19:24:58 -0500

    refresh

--- Search_FoodName_StoreName.sql
+++ Search_FoodName_StoreName.sql
@@ -1,4 +1,9 @@
-SELECT DISTINCT F.name, F.category, A.price, S.name 
+CREATE VIEW search_result AS
+SELECT DISTINCT F.food_id, F.name AS food_name, F.category, A.price, S.store_id, S.name AS store_name
 FROM foods F, available A, stores S
-WHERE (F.name = "FoodNameInput") AND (S.name = "StoreNameInput") AND (A.quantity > 0);
-#foodName and StoreName is input
+WHERE (F.name = "Minute Maid Orange Juice") AND (S.name = "Target") AND (A.quantity > 0) AND (F.food_id = A.food_id) AND (S.store_id = A.store_id);
+
+SELECT food_name, category, price, store_name
+FROM search_result;
+
+DROP VIEW search_result;
