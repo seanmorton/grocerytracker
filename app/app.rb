6efe1d422ec38541e7f1d2583dd3f013493d53fb
megus <smmorton@wisc.edu>
2015-05-06 22:48:57 -0500

    Food CRUD done

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,12 @@ class GroceryTracker < Sinatra::Base
   enable :sessions
 
   before do
-    pass if %w(login authenticate).include? request.path_info.split('/')[1]
+    pass if %w(login authenticate register).include? request.path_info.split('/')[1]
     redirect to('/login') if session[:user_id].nil?
   end
 
   get '/' do
-    'Hello!'
+    `fortune`
   end
 
   get '/login' do
@@ -21,18 +21,54 @@ class GroceryTracker < Sinatra::Base
 
   get '/logout' do
     session[:user_id] = nil
-    redirect to('/login')
+    redirect to('/pantry')
   end
 
   post '/login' do
     session[:user_id]  = get_user_id(params[:username], params[:password])
-    redirect to('/all_users')
+    redirect to('/pantry')
   end
 
-  get '/all_users' do
-    @all_users = all_users
-    haml :all_users
+  get '/register' do
+    haml :register, :layout => nil
   end
 
+  post '/register' do
+    session[:user_id] = new_user(params[:username], params[:password], params[:name])
+    redirect to('/pantry')
+  end
+
+  get '/pantry' do
+    haml :pantry
+  end
+
+  get '/all_foods' do
+    @all_foods = all_foods
+    haml :all_foods
+  end
+
+  get '/new_food' do
+    haml :new_food
+  end
+
+  post '/new_food' do
+    new_food(params[:name], params[:category], params[:description])
+    redirect to('/all_foods')
+  end
+
+  get '/edit_food' do
+    @food = get_food(params[:food_id])
+    haml :edit_food
+  end
+
+  post '/edit_food' do
+    edit_food(params[:food_id], params[:name], params[:category], params[:description])
+    redirect to('/all_foods')
+  end
+
+  get '/delete_food' do
+    delete_food(params[:food_id])
+    redirect to('/all_foods')
+  end
 
 end
--- app/db.rb
+++ app/db.rb
@@ -3,11 +3,36 @@ require 'mysql2'
 DBCONN = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "password", :database => 'grocery_tracker')
 DBCONN.query_options.merge!(:symbolize_keys => true)
 
-def all_users
-  DBCONN.query("SELECT * FROM users")
-end
+
 
 def get_user_id(username, password)
   tuple = DBCONN.query("SELECT user_id FROM users WHERE user_name = '#{username}' AND password = '#{password}'").first
   tuple.nil? ? nil : tuple[:user_id]
+end
+
+def new_user(username, password, name)
+  DBCONN.query("INSERT INTO users (user_name, password, name) VALUES ('#{username}', '#{password}', '#{name}')")
+  tuple = DBCONN.query("SELECT user_id FROM users WHERE user_name = '#{username}' AND password = '#{password}'").first
+  tuple[:user_id]
+end
+
+def all_foods
+  DBCONN.query("SELECT * FROM foods")
+end
+
+def get_food(food_id)
+  DBCONN.query("SELECT * FROM foods WHERE food_id = #{food_id}").first
+end
+
+def new_food(name, category, description)
+  DBCONN.query("INSERT INTO foods (name, category, description) VALUES ('#{name}', '#{category}', '#{description}')")
+end
+
+def edit_food(food_id, name, category, description)
+  food = DBCONN.query("SELECT * FROM foods WHERE food_id = #{food_id}").first
+  DBCONN.query("UPDATE foods SET name ='#{name || food[:name] }', category = '#{category || food[:category] }', description = '#{description || food[:description]}' WHERE food_id = #{food_id}")
+end
+
+def delete_food(food_id)
+  DBCONN.query("DELETE FROM foods WHERE food_id = #{food_id}")
 end
